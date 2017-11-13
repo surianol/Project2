1b615da1fcf599f74bd5fbe1e961ba9dfed7c54b
Matthew Blanski <mblanski@iit.edu>
2017-11-12 20:09:43 -0600

    Added term registration load check for embedded functions

--- js/registration.js
+++ js/registration.js
@@ -1,13 +1,25 @@
 function iitRegistration(){
-  console.log( "ready!" );
-  console.log( "loading terms!" );
-  $.getJSON( "json/terms.json", function ( termdata ) {
-    var terms = [];
-    $.each(termdata, function( key, val ) {
-      console.log(key);
-    });
-  });
-  console.log( "loading courses!" );
+	console.log("Defining Functions");
+	//Term load and parsing
+	function parseTerms() {
+		console.log("Loading Term Json");
+		$.getJSON( "json/terms.json", function ( termdata ) {
+			console.log("Term Json Loaded.");
+			var termArray = [];
+			$.each(termdata, function( termkey, termvalue ) {
+				if(termkey !== "terms") {
+					console.log("Term Data Fails Validation!");
+					return;
+				}
+				console.log("Term Data Passes Validation!");
+				termArray = termvalue;
+			});
+		});
+	}
+	console.log( "Ready!" );
+	console.log( "Loading Terms!" );
+	parseTerms();
+	console.log( "Loading Courses!" );
 };
 
 // Handle document ready
