194269f1d578deff9c9508dd630bdf4ce44f9335
Matthew Blanski <mblanski@iit.edu>
2017-11-12 21:09:45 -0600

    Changed data model for simple name pulls to ease parsing of files.

--- js/registration.js
+++ js/registration.js
@@ -9,15 +9,16 @@ function iitRegistration(){
 	debugLog("Debug Mode Active.");
 
 	//Define Functions
-	debugLog("Defining Functions.");
-
+	debugLog("Defining Objects");
+	
+	var termArray = [];
 	//Term load and parsing
 	function parseTerms() {
 		debugLog("Loading Term Json");
 		$.getJSON( "json/terms.json", function ( termdata ) {
 			debugLog("Term Json Loaded.");
 			var tempTermArray = [];
-			var termArray = [];
+			
 			debugLog("Capture Term Data");
 			$.each(termdata, function( termkey, termvalue ) {
 				if(termkey !== "terms") {
@@ -37,15 +38,23 @@ function iitRegistration(){
 				} else if (Date.parse(termdefinition.StartDate) < Date.now()) {
 					debugLog(termdefinition.Name);
 					debugLog("Registration");
+					termArray.push({name:termdefinition.Name,simplename:termdefinition.SimpleName,mode:'Registration'});
+					debugLog(termArray);
 				} else if (Date.parse(termdefinition.AddDropDate) < Date.now()) {
 					debugLog(termdefinition.Name);
-					debugLog("Add Drop");
+					debugLog("AddDrop");
+					termArray.push({name:termdefinition.Name,simplename:termdefinition.SimpleName,mode:'AddDrop'});
+					debugLog(termArray);
 				} else if (Date.parse(termdefinition.WithdrawDate) < Date.now()) {
 					debugLog(termdefinition.Name);
 					debugLog("Withdraw");
+					termArray.push({name:termdefinition.Name,simplename:termdefinition.SimpleName,mode:'Withdraw'});
+					debugLog(termArray);
 				} else if (Date.parse(termdefinition.EndDate) < Date.now()) {
 					debugLog(termdefinition.Name);
 					debugLog("No changes");
+					termArray.push({name:termdefinition.Name,simplename:termdefinition.SimpleName,mode:'NoChange'});
+					debugLog(termArray);
 				}
 			});
 			debugLog("Term Iteration Done");
