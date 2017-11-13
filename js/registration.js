3745436fabea724bdeead4333f1460e693e8497e
Matthew Blanski <mblanski@iit.edu>
2017-11-12 19:57:52 -0600

    Got json to parse terms!

--- js/registration.js
+++ js/registration.js
@@ -1,5 +1,13 @@
 function iitRegistration(){
   console.log( "ready!" );
+  console.log( "loading terms!" );
+  $.getJSON( "json/terms.json", function ( termdata ) {
+    var terms = [];
+    $.each(termdata, function( key, val ) {
+      console.log(key);
+    });
+  });
+  console.log( "loading courses!" );
 };
 
 // Handle document ready
