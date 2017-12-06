9ef80033da20bea219a10f6b820d8afd49949986
Matthew Blanski <mblanski@iit.edu>
2017-12-05 21:19:32 -0600

    Fixed decription handling for firefox...

--- javascript/registration.js
+++ javascript/registration.js
@@ -176,7 +176,9 @@ function iitRegistration(){
       $(this).removeClass("registration_event_unbound");
     });
     $("input.coursedata_description_button_unbound").each(function() {
-      $(this).on("click",{id: $(this).attr('id') }, toggleDescription);
+      $(this).on("click",{id: $(this).attr('id') }, function(){
+		toggleDescription(event)
+      });
       $(this).removeClass("coursedata_description_button_unbound");
     });
     $("input.register_arrow").each(function() {
@@ -223,12 +225,12 @@ function iitRegistration(){
     debugLog("firing description hide/show!");
     event.stopPropagation();
 	var target = event.target;
-    var descriptionItem = $(this).parent().find('span.coursedata_description');
+    var descriptionItem = $(target).parent().find('span.coursedata_description');
     if( $(descriptionItem).is(':hidden')) {
-      $(this).attr("value","Less ...");
+      $(target).attr("value","Less ...");
       $(descriptionItem).show();
     } else {
-      $(this).attr("value","...");
+      $(target).attr("value","...");
       $(descriptionItem).hide();
     }
   }
